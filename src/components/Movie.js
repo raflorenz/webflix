ba102caf77c67662b21da4747ccde576f48920d8
Raffy Lorenzana <rjlorenz931@gmail.com>
2022-04-02 02:10:37 +0800

    implement show movie details functionality

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,13 @@
-function Movie({ movie }) {
+function Movie({ movie, clickHandler, activeMovie }) {
     return (
         <div className="movie">
-            <img src={movie['im:image'][2]['label']} />
-            <p>{movie['im:name']['label']}</p>
+            <img src={movie['im:image'][2]['label']} onClick={clickHandler} />
+            {activeMovie && (
+                <div>
+                    <p>{activeMovie['im:name']['label']}</p>
+                    <p>{activeMovie['summary']['label']}</p>
+                </div>
+            )}
         </div>
     );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,18 @@
+import { useState } from 'react'
 import Movie from './Movie'
 
 function Movies({ movies }) {
+    const [active, setActive] = useState(null)
+    
     return (
         <section className="movies">
             {movies.map(movie => (
-                <Movie key={movie['id']['attributes']['im:id']} movie={movie} />
+                <Movie 
+                    key={movie['id']['attributes']['im:id']} 
+                    movie={movie} 
+                    clickHandler={() => setActive(movie['id']['attributes']['im:id'])}
+                    activeMovie={active === movie['id']['attributes']['im:id'] ? movie : null}
+                />
             ))}
         </section>
     );
