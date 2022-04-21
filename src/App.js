6f0089b0bdbbe2f3ce92568aad0a4815bbd2b960
Raffy Lorenzana <rjlorenz931@gmail.com>
2022-04-21 22:04:26 +0800

    add see all popular movies and react video player

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import { useState, useEffect } from 'react'
 import Movies from './components/Movies'
+import ReactPlayer from 'react-player'
 import './App.css'
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [limit, setLimit] = useState(20);
+
+  const videoLinks = movies.map(movie => movie['link'][1]['attributes']['href'])
 
   useEffect(() => {
     // fetch('https://itunes.apple.com/us/rss/topmovies/limit=100/json').then(response => response.json()).then(data => console.log(data.feed.entry))
     (async function fetchMovies() {
-      const response = await fetch('https://itunes.apple.com/us/rss/topmovies/limit=100/json');
+      const response = await fetch(`https://itunes.apple.com/us/rss/topmovies/limit=${limit}/json`);
       const data = await response.json();
 
       setMovies(data.feed.entry);
     })();
-  }, []);
+  }, [limit]);
 
   return (
     <div className="app">
       <h1 className="heading">Webflix</h1>
+      <ReactPlayer url={videoLinks[0]} playing muted width="100%" height="100%" />
+      <h2>Popular Movies {limit === 20 && <span onClick={() => setLimit(100)}>See All</span>}</h2>
       <Movies movies={movies} />
     </div>
   );
