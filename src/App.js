d9607da0469823f3a2399f1fa06920b09413da31
Raffy Lorenzana <rjlorenz931@gmail.com>
2022-04-29 00:32:02 +0800

    implement featured movie

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { useState, useEffect } from 'react'
 import Movies from './components/Movies'
-import ReactPlayer from 'react-player'
+import FeaturedMovie from './components/FeaturedMovie'
 import './App.css'
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [limit, setLimit] = useState(20);
-
-  const videoLinks = movies.map(movie => movie['link'][1]['attributes']['href'])
 
   useEffect(() => {
     // fetch('https://itunes.apple.com/us/rss/topmovies/limit=100/json').then(response => response.json()).then(data => console.log(data.feed.entry))
@@ -22,7 +20,7 @@ function App() {
   return (
     <div className="app">
       <h1 className="heading">Webflix</h1>
-      <ReactPlayer url={videoLinks[0]} playing muted width="100%" height="100%" />
+      <FeaturedMovie movies={movies} />
       <h2>Popular Movies {limit === 20 && <span onClick={() => setLimit(100)}>See All</span>}</h2>
       <Movies movies={movies} />
     </div>
---
+++ src/components/FeaturedMovie.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+import ReactPlayer from 'react-player'
+
+function FeaturedMovie({ movies }) {
+    const [index, setIndex] = useState(0)
+    const featuredMovie = movies[index]
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const randomNumber = Math.floor(Math.random() * ((movies.length - 1) - 0)) + 0
+            setIndex(randomNumber)
+        }, 30000);
+
+        return () => clearInterval(interval);
+    }, [movies])
+    
+    return (
+        <section className="featured-movie">
+            <ReactPlayer url={featuredMovie && featuredMovie['link'][1]['attributes']['href']} playing muted width="100%" height="750px" />
+            <div className="featured-movie-content">
+                <h1 className="featured-movie-title">{featuredMovie && featuredMovie['im:name']['label']}</h1>
+                <ul className="featured-movie-list">
+                    <li>{featuredMovie && featuredMovie['category']['attributes']['label']}</li>
+                    <li>{featuredMovie && featuredMovie['im:releaseDate']['label'].split('-')[0]}</li>
+                </ul>
+                <p className="featured-movie-summary">{featuredMovie && featuredMovie['summary']['label']}</p>
+                <a href={featuredMovie && featuredMovie['link'][0]['attributes']['href']} className="featured-movie-button" target="_blank">Watch Now</a>
+            </div>
+        </section>
+    );
+}
+
+export default FeaturedMovie
