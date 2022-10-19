7a33908bb56a58b31e8c1ca81437bed5771533ce
Raffy Lorenzana <rjlorenz931@gmail.com>
2022-10-19 23:15:47 +0800

    close movie popupu when clicked outside

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,8 @@
 function Movie({ movie, clickHandler, activeMovie }) {
     return (
         <div className={`movie ${activeMovie ? 'is-active' : ''}`}>
-            <div className="movie-front">
-                <img src={movie['im:image'][2]['label']} onClick={clickHandler} />
+            <div className="movie-front" onClick={clickHandler}>
+                <img src={movie['im:image'][2]['label']} />
                 <span className="movie-release-date">{movie['im:releaseDate']['label'].split('-')[0]}</span>
                 <span className="movie-category">{movie['category']['attributes']['label']}</span>
             </div>
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,22 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Movie from './Movie'
 
 function Movies({ movies }) {
     const [active, setActive] = useState(null)
 
     const showMovieDetails = index => {
-        if (active === index) return setActive(null)
+        if (active) return setActive(null)
         setActive(index)
     }
+
+    const ref = useRef()
+
+    useEffect(() => {
+        const checkIfClickedOutside = e => {
+          // if the popup is open and the clicked target is not within the popup, then close the popup
+          if (active && ref.current && !ref.current.contains(e.target)) {
+            setActive(null)
+          }
+        }
+    
+        document.addEventListener("click", checkIfClickedOutside)
+    
+        return () => {
+          // cleanup the event listener
+          document.removeEventListener("click", checkIfClickedOutside)
+        }
+    }, [active])
     
     return (
-        <section className="movies">
+        <section className="movies" ref={ref}>
             {movies.map((movie, index) => (
                 <Movie 
                     key={movie['id']['attributes']['im:id']} 
                     movie={movie} 
-                    clickHandler={() => showMovieDetails(index)}
-                    activeMovie={active === index}
+                    clickHandler={() => showMovieDetails(index+1)}
+                    activeMovie={active === index+1}
                 />
             ))}
         </section>
