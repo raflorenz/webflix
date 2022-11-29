7c6cd397f0e80894db724e5430289316109ab39c
Raffy Lorenzana <rjlorenz931@gmail.com>
2022-11-30 03:33:19 +0800

    implement search functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Movies from './components/Movies'
 import FeaturedMovie from './components/FeaturedMovie'
+import Search from './components/Search'
 import './App.css'
 
 function App() {
@@ -8,7 +9,6 @@ function App() {
   const [limit, setLimit] = useState(20)
 
   useEffect(() => {
-    // fetch('https://itunes.apple.com/us/rss/topmovies/limit=100/json').then(response => response.json()).then(data => console.log(data.feed.entry))
     (async function fetchMovies() {
       const response = await fetch(`https://itunes.apple.com/us/rss/topmovies/limit=${limit}/json`);
       const data = await response.json();
@@ -25,6 +25,7 @@ function App() {
   return (
     <div className="app">
       <h1 className="heading">Webflix</h1>
+      <Search />
       <FeaturedMovie movies={movies} />
       <h2>Popular Movies {limit === 20 && <span onClick={() => setLimit(100)}>See All</span>}</h2>
       <Movies movies={movies} />
