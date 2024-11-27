0787308050042206c672e084c443e46af2dd8681
raflorenz <rjlorenz931@gmail.com>
2024-11-27 16:08:30 +0800

    add trending nonw movies/tv shows, update grid markup and styling

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,22 @@ export default async function Home() {
     },
   };
 
+  const trendingPage1 = await fetch(
+    "https://api.themoviedb.org/3/trending/all/day?language=en-US&page=1",
+    options
+  )
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+
+  const trendingPage2 = await fetch(
+    "https://api.themoviedb.org/3/trending/all/day?language=en-US&page=2",
+    options
+  )
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+
+  const trending = [...trendingPage1.results, ...trendingPage2.results];
+
   const popularMovies = await fetch(
     "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
     options
@@ -26,17 +42,24 @@ export default async function Home() {
 
   return (
     <div className="container mx-auto px-8">
-      <h1 className="brand">Webflix</h1>
+      <h1 className="my-8 text-6xl text-[#e50914] uppercase">Webflix</h1>
 
-      <h2 className="heading">Popular Movies</h2>
-      <div className="movies">
+      <h2 className="mt-16 mb-8 text-4xl text-[#e50914]">Trending Now</h2>
+      <div className="grid grid-rows-2 grid-flow-col gap-2 overflow-x-auto pb-2">
+        {trending.map((movie) => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
+      </div>
+
+      <h2 className="mt-16 mb-8 text-4xl text-[#e50914]">Popular Movies</h2>
+      <div className="grid grid-rows-2 grid-flow-col gap-2 overflow-x-auto pb-2">
         {popularMovies.results.map((movie) => (
           <Movie key={movie.id} movie={movie} />
         ))}
       </div>
 
-      <h2 className="heading">Top Rated Movies</h2>
-      <div className="movies">
+      <h2 className="mt-16 mb-8 text-4xl text-[#e50914]">Top Rated Movies</h2>
+      <div className="grid grid-rows-2 grid-flow-col gap-2 overflow-x-auto pb-2">
         {topRatedMovies.results.map((movie) => (
           <Movie key={movie.id} movie={movie} />
         ))}
--- components/movie.tsx
+++ components/movie.tsx
@@ -1,20 +1,27 @@
 import { formatDate } from "@/lib/utils";
-import { Star } from "lucide-react";
+import { Star, Film, TvMinimal } from "lucide-react";
 
 export default async function Movie({ movie }) {
   return (
-    <div className="movie">
-      <div className="movie-front">
-        <img
-          src={`https://image.tmdb.org/t/p/w220_and_h330_face/${movie.poster_path}`}
-        />
-        <span className="movie-rating">
-          <Star size={15} />
-          <span>{movie.vote_average.toFixed(1)}</span>
-        </span>
-        <span className="movie-release-date">
-          {formatDate(movie.release_date)}
-        </span>
+    <div className="media relative w-[200px]">
+      <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} />
+      <div className="flex items-center gap-[3px] top-2 left-2">
+        <Star size={16} />
+        <span>{movie.vote_average ? movie.vote_average.toFixed(1) : "NR"}</span>
+      </div>
+      <div className="top-2 right-2">
+        {movie.media_type === "tv" ? (
+          <TvMinimal size={16} />
+        ) : (
+          <Film size={16} />
+        )}
+      </div>
+      <div className="bottom-2 left-2">
+        {formatDate(movie.release_date || movie.first_air_date)}
+        {movie.first_air_date &&
+          ` - ${
+            movie.last_air_date ? formatDate(movie.last_air_date) : "Present"
+          }`}
       </div>
     </div>
   );
