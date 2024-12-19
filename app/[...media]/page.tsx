018efd699f9b57f654cfbab6622486435c0bf458
raflorenz <rjlorenz931@gmail.com>
2024-12-19 20:53:59 +0800

    fix some error related to dynamicIO

--- app/[...media]/page.tsx
+++ app/[...media]/page.tsx
@@ -1,6 +1,12 @@
+import { unstable_cacheLife as cacheLife } from "next/cache";
+import { Suspense } from "react";
 import { fetchMediaDetails } from "@/lib/api";
+import { Skeleton } from "@/components/ui/skeleton";
 
-export default async function Page({ params }) {
+async function MediaDetails({ params }) {
+  "use cache";
+  cacheLife("hours");
+
   const id = (await params).media[1];
   const media_type = (await params).media[0];
 
@@ -8,12 +14,27 @@ export default async function Page({ params }) {
 
   return (
     <div className="container mx-auto px-8">
-      <div className="flex justify-between items-center">
-        <h1 className="my-8 text-6xl text-[#e50914] uppercase">
-          {details.title || details.name}
-        </h1>
-        <p>{details.overview}</p>
-      </div>
+      <h1 className="my-8 text-6xl text-[#e50914] uppercase">
+        {details.title || details.name}
+      </h1>
+      <p>{details.overview}</p>
+    </div>
+  );
+}
+
+export default async function Page({ params }) {
+  return (
+    <Suspense fallback={<MediaDetailsSkeleton />}>
+      <MediaDetails params={params} />
+    </Suspense>
+  );
+}
+
+function MediaDetailsSkeleton() {
+  return (
+    <div className="container mx-auto px-8">
+      <Skeleton className="h-10 w-[50%] my-8" />
+      <Skeleton className="h-40 w-full" />
     </div>
   );
 }
